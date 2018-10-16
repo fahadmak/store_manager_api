f2de47666afdc628b745d89f3bd9ff24a2e5b05c
faraqhan <f.faraqhan@gmail.com>
2018-10-17 00:55:03 +0300

    refactor validate function [#161249022]

--- app/validate.py
+++ app/validate.py
@@ -3,8 +3,6 @@ import re
 
 def validate(name, price):
     """A method to Check if name and price are of the correct form"""
-    if re.compile('([a-zA-z]*)').match(str(name)):
-        return False
-    if re.compile('([1-9][0-9]*)').match(price):
-        return False
-    return True
+    if re.compile('^[a-zA-z]+').match(str(name)) and re.compile('([1-9][0-9]*)').match(str(price)):
+        return True
+    return False
