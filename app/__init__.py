9ba322ed84dd8b6d3b6c9910d6182ff3f967f13b
faraqhan <f.faraqhan@gmail.com>
2018-10-17 01:01:48 +0300

    create tests for create_user and to_json

--- app/__init__.py
+++ app/__init__.py
@@ -5,10 +5,8 @@ from config import app_config
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(app_config[config_name])
-    from .user.view import user as user_blueprint
-    app.register_blueprint(user_blueprint)
+    from .product.view import product as product_blueprint
+    app.register_blueprint(product_blueprint)
     return app
 
-
-app = create_app("development")
 
--- app/product/view.py
+++ app/product/view.py
@@ -7,16 +7,23 @@ product = Blueprint("product", __name__)
 
 @product.route("/api/v1/products", methods=["POST"])
 def add_product():
-    data = request.json
-    name = data.get("name")
-    price = data.get("price")
-    if not all([name, price]):
-        return jsonify({"message": "please fill missing fields"}), 400
-    valid = validate(name, price)
-    if not valid:
-        return jsonify({"message": "incorrect information"}), 400
-    productId = max([product.productId for product in products]) + 1 if products else 1
-    product = Product(productId, name, price)
-    products.append(product)
-    return jsonify({"message": str(product)}), 201
+    try:
+        data = request.json
+        name = data.get("name")
+        price = data.get("price")
+        if not all([name, price]):
+            return jsonify({"message": "please fill missing fields"}), 400
+        valid = validate(name, price)
+        if not valid:
+            return jsonify({"message": "incorrect information"}), 400
+        if name in [product.name for product in products]:
+            return jsonify({"message": f"{name} already exists"}), 409
+        productId = max([product.productId for product in products]) + 1 if products else 1
+        product = Product(productId, name, price)
+        products.append(product)
+        return jsonify({"message": str(product)}), 201
+    except:
+        return jsonify({"message": "Oops something went wrong"}), 500
+
+
 
