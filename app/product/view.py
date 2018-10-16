845b6ad66b111aa8aaef158e53547bcad0192c86
faraqhan <f.faraqhan@gmail.com>
2018-10-16 23:50:03 +0300

    refactor add_product function to check for empty fields [#161249022]

--- app/product/view.py
+++ app/product/view.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, jsonify, request, json
 
 from app.model import Product, products
-
+from app.validate import validate
 product = Blueprint("product", __name__)
 
 
@@ -10,6 +10,8 @@ def add_product():
     data = request.json
     name = data.get("name")
     price = data.get("price")
+    if not all([name, price]):
+        return jsonify({"message": "please fill missing fields"}), 400
     productId = max([product.productId for product in products]) + 1 if products else 1
     product = Product(productId, name, price)
     products.append(product)
