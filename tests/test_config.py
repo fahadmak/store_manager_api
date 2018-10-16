b8234b43e94e08c269daec198ad9e8bfdc4d8b32
faraqhan <f.faraqhan@gmail.com>
2018-10-16 18:15:05 +0300

    create tests for configurations

---
+++ tests/test_config.py
@@ -0,0 +1,15 @@
+import unittest
+from app import create_app
+
+
+class TestDevelopmentConfig(unittest.TestCase):
+
+    def test_app_is_development(self):
+        app = create_app("development")
+        self.assertTrue(app.config['DEBUG'] is True)
+
+    def test_app_is_testing(self):
+        app = create_app("testing")
+        self.assertTrue(app.config['DEBUG'] is True)
+        self.assertTrue(app.config['TESTING'] is True)
+
