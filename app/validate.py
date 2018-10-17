b4cb771abbb768bd8e0ea673eea7d1b377522311
faraqhan <f.faraqhan@gmail.com>
2018-10-18 01:00:01 +0300

    fix pattern to check name [#161249190]

--- app/validate.py
+++ app/validate.py
@@ -3,6 +3,6 @@ import re
 
 def validate(name, price):
     """A method to Check if name and price are of the correct form"""
-    if re.compile('^[a-zA-z]+').match(str(name)) and re.compile('([1-9][0-9]*)').match(str(price)):
+    if re.compile('^[a-zA-Z]+').match(str(name)) and re.compile('([1-9][0-9]*)').match(str(price)):
         return True
     return False
