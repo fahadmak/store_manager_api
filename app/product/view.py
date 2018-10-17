f4d601b37c7b6a717cdb1e7a7e83882ae8d2612e
faraqhan <f.faraqhan@gmail.com>
2018-10-17 17:34:48 +0300

    remove try except statements

--- app/product/view.py
+++ app/product/view.py
@@ -7,32 +7,27 @@ product = Blueprint("product", __name__)
 
 @product.route("/api/v1/products", methods=["POST"])
 def add_product():
-    try:
-        data = request.json
-        name = data.get("name")
-        price = data.get("price")
-        if not all([name, price]):
-            return jsonify({"message": "please fill missing fields"}), 400
-        valid = validate(name, price)
-        if not valid:
-            return jsonify({"message": "incorrect information"}), 400
-        if name in [product.name for product in products]:
-            return jsonify({"message": f"{name} already exists"}), 409
-        productId = max([product.productId for product in products]) + 1 if products else 1
-        product = Product(productId, name, price)
-        products.append(product)
-        return jsonify({"message": str(product)}), 201
-    except:
-        return jsonify({"message": "Oops something went wrong"}), 500
+    data = request.json
+    name = data.get("name")
+    price = data.get("price")
+    if not all([name, price]):
+        return jsonify({"message": "please fill missing fields"}), 400
+    valid = validate(name, price)
+    if not valid:
+        return jsonify({"message": "incorrect information"}), 400
+    if name in [product.name for product in products]:
+        return jsonify({"message": f"{name} already exists"}), 409
+    productId = max([product.productId for product in products]) + 1 if products else 1
+    product = Product(productId, name, price)
+    products.append(product)
+    return jsonify({"message": str(product)}), 201
+
 
 
 @product.route("/api/v1/products", methods=["GET"])
 def get_all_products():
-    try:
-        if not products:
-            error = json.dumps({"message": "There currently no products"})
-            return jsonify(error), 200
-        all_products = json.dumps([product.to_json() for product in products])
-        return jsonify({"products": all_products}), 200
-    except:
-        return jsonify({"message": "Oops something went wrong"}), 500
+    if not products:
+        error = json.dumps({"message": "There currently no products"})
+        return jsonify(error), 200
+    all_products = json.dumps([product.to_json() for product in products])
+    return jsonify({"products": all_products}), 200
