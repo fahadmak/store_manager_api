6fd751a98fbd5181d4758399249cd3c2d8c48ff8
faraqhan <f.faraqhan@gmail.com>
2018-10-19 07:12:04 +0300

    add exceptions + refactor code

--- app/product/view.py
+++ app/product/view.py
@@ -1,8 +1,10 @@
-from flask import Blueprint, jsonify, request, json
+from flask import Blueprint, jsonify, request
 
-from app.model import Product, products
+from app.models.model import Product, products
+from app.models.error_model import InvalidUsage, InternalServerError
 from app.validate import validate
 from app.utils import find_product
+
 
 product = Blueprint("product", __name__)
 
@@ -11,40 +13,62 @@ product = Blueprint("product", __name__)
 def add_product():
     """A method adds product instance to products list"""
     if request.content_type != "application/json":
-        return jsonify({'message': "Invalid content type"}), 400
+        raise InvalidUsage("Invalid content type", 400)
     data = request.json
     name = data.get("name")
     price = data.get("price")
-    if not all([name, price]):
-        return jsonify({"message": "please fill missing fields"}), 400
-    valid = validate(name, price)
-    if not valid:
-        return jsonify({"message": "incorrect information"}), 400
-    found_product = find_product(name)
-    if found_product:
-        return jsonify({"message": f"{name} already exists"}), 400
-    productId = max([product.productId for product in products]) + 1 if products else 1     # create productId
-    product = Product(productId, name, price)
-    products.append(product)
-    return jsonify({"message": str(product)}), 201
+    try:
+        if not all([name, price]):
+            raise InvalidUsage("please fill missing fields", 400)
+        valid = validate(name, price)
+        if not valid:
+            raise InvalidUsage("incorrect information", 400)
+        found_product = find_product(name)
+        if found_product:
+            raise InvalidUsage(f"{name} already exists", 400)
+        item_id = max([item.productId for item in products]) + 1 if products else 1     # create productId
+        item = Product(item_id, name, price)
+        products.append(item)
+        return jsonify({"message": str(item)}), 201
+    except InternalServerError:
+        raise InternalServerError
 
 
 @product.route("/api/v1/products", methods=["GET"])
 def get_all_products():
     """A method returns a products dictionary"""
-    if not products:
-        return jsonify({"message": "There currently no products"}), 200
-    all_products = [product.to_json() for product in products]
-    return jsonify({"products": all_products}), 200
+    try:
+        if not products:
+            raise InvalidUsage("There currently no products", 200)
+        all_products = [item.to_json() for item in products]
+        return jsonify({"products": all_products}), 200
+    except InternalServerError:
+        raise InternalServerError
 
 
-@product.route("/api/v1/products/<int:productId>", methods=["GET"])
-def get_product_by_id(productId):
+@product.route("/api/v1/products/<int:product_id>", methods=["GET"])
+def get_product_by_id(product_id):
     """A method returns a product whose id is passed as an argument"""
-    product = [product.to_json() for product in products if product.productId == productId] if products else False
-    if not product:
-        error = {"message": f"product of ID {productId} does not exist"}
-        return jsonify({"product": error}), 404
-    product = product[0]
-    return jsonify({"product": product}), 200
+    try:
+        found_product = [item.to_json() for item in products if item.productId == product_id] if products else False
+        if not found_product:
+            raise InvalidUsage(f"product of ID {product_id} does not exist", 404)
+        found_product = found_product[0]
+        return jsonify({"product": found_product}), 200
+    except InternalServerError:
+        raise InternalServerError
+
+
+@product.errorhandler(InvalidUsage)
+def handle_invalid_usage(error):
+    response = jsonify(error.to_dict())
+    response.status_code = error.status_code
+    return response
+
+
+@product.errorhandler(InternalServerError)
+def internal_server_error():
+    response = jsonify({"message": InternalServerError.message})
+    response.status_code = InternalServerError.status_code
+    return response
 
--- app/sale/view.py
+++ app/sale/view.py
@@ -1,44 +1,70 @@
-from flask import Blueprint, jsonify, request, json
+from flask import Blueprint, jsonify, request
 
-from app.model import Sale, sales
+from app.models.model import Sale, sales, products
+from app.models.error_model import InvalidUsage, InternalServerError
+
 from app.utils import find_product
+
+
 sale = Blueprint('sale', __name__)
 
 
 @sale.route("/api/v1/sales", methods=["POST"])
 def add_sale():
     if request.content_type != "application/json":
-        return jsonify({"error": "Invalid content type"}), 400
+        raise InvalidUsage("Invalid content type", 400)
     data = request.json
     cart = data.get('cart')
-    if not cart:
-        return jsonify({"error": "please fill missing fields"}), 400
-    if not isinstance(cart, dict):
-        return jsonify({"error": "please input correct format like {'corn': 6000, 'fish': 12300}"}), 400
-    cart_products = [product for product in cart]
-    unavailable = [cart_product for cart_product in cart_products if find_product(cart_product) is False]
-    if not unavailable:
-        sold_id = max([sold.saleId for sold in sales]) + 1 if sales else 1
-        sold = Sale(sold_id, cart)
-        sales.append(sold)
-        return jsonify({"message": str(sold)}), 201
-    return jsonify({"error": f"{unavailable} products can not be found"}), 404
+    try:
+        if not cart:
+            raise InvalidUsage("please fill missing fields", 400)
+        if not isinstance(cart, dict):
+            raise InvalidUsage("please input correct format like {'corn': 6000, 'fish': 12300}", 400)
+        unavailable = [item for item in cart if item not in [product.name for product in products]]
+        if not unavailable:
+            sold_id = max([sold.saleId for sold in sales]) + 1 if sales else 1
+            sold = Sale(sold_id, cart)
+            sales.append(sold)
+            return jsonify({"message": str(sold)}), 201
+        raise InvalidUsage(f"{unavailable} products can not be found", 404)
+    except InternalServerError:
+        raise InternalServerError
 
 
 @sale.route("/api/v1/sales", methods=["GET"])
 def get_all_sales():
-    if not sales:
-        return jsonify({"error": "They are currently no sales"}), 404
-    json_sales = []
-    for sold in sales:
-        json_sales.append(sold.to_json())
-    return jsonify({"sales": json_sales}), 200
+    try:
+        if not sales:
+            raise InvalidUsage("They are currently no sales", 404)
+        json_sales = []
+        for sold in sales:
+            json_sales.append(sold.to_json())
+        return jsonify({"sales": json_sales}), 200
+    except InternalServerError:
+        raise InternalServerError
 
 
-@sale.route("/api/v1/sales/<int:saleId>", methods=["GET"])
-def get_sale_by_id(saleId):
-    for sold in sales:
-        if sold.saleId == saleId:
-            return jsonify({"sale": sold.to_json()}), 200
-    else:
-        return jsonify({"error": f"Sale Record of ID {saleId} does not exist"}), 404
+@sale.route("/api/v1/sales/<int:sale_id>", methods=["GET"])
+def get_sale_by_id(sale_id):
+    try:
+        for sold in sales:
+            if sold.saleId == sale_id:
+                return jsonify({"sale": sold.to_json()}), 200
+        else:
+            return jsonify({"error": f"Sale Record of ID {sale_id} does not exist"}), 404
+    except InternalServerError:
+        raise InternalServerError
+
+
+@sale.errorhandler(InvalidUsage)
+def handle_invalid_usage(error):
+    response = jsonify(error.to_dict())
+    response.status_code = error.status_code
+    return response
+
+
+@sale.errorhandler(InternalServerError)
+def internal_server_error():
+    response = jsonify({"message": InternalServerError.message})
+    response.status_code = InternalServerError.status_code
+    return response
