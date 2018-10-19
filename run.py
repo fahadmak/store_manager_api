e5506ca77127806780d6ff3bab7f7bcf08951708
faraqhan <f.faraqhan@gmail.com>
2018-10-19 08:27:31 +0300

    add configurations for heroku

--- run.py
+++ run.py
@@ -2,7 +2,8 @@ import os
 
 from app import create_app
 
-port = int(os.environ.get('PORT', 5000))
+
 app = create_app("development")
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=port)
+
+port = int(os.environ.get('PORT', 33507))
+app.run(host='0.0.0.0', port=port)
